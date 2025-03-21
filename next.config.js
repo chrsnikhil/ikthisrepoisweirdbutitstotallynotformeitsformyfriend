f3bb73c87e6de3d44f5a24a26041a697144e7fc3
Nikhil <chrsnikhil@gmail.com>
2025-03-21 22:37:43 +0530

    Error fix

--- next.config.js
+++ next.config.js
@@ -4,15 +4,7 @@ const nextConfig = {
   swcMinify: true,
   images: {
     domains: ['fonts.googleapis.com'],
-  },
-  // Ensure we handle CSS imports properly
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
-    });
-    return config;
-  },
+  }
 }
 
 module.exports = nextConfig 
